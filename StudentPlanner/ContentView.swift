8ec08bd65e6d2123ae0557c521ac0615a5b1adf2
cabsalvador <salvadorcabrera@csus.edu>
2022-04-19 10:09:33 -0700

    Initial Commit

---
+++ StudentPlanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudentPlanner
+//
+//  Created by Sal on 4/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudentPlanner/StudentPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentPlannerApp.swift
+//  StudentPlanner
+//
+//  Created by Sal on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct StudentPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
